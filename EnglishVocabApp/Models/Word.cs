273b7ffd8981c193d26882e7b30dfc6944e5b26d
Ivanytska Anna <aniaiv12345@gmail.com>
2025-02-08 13:39:45 +0200

    created class Word

---
+++ EnglishVocabApp/Models/Word.cs
@@ -0,0 +1,19 @@
+﻿namespace EnglishVocabApp.Models
+{
+    public class Word
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+        // a new class must be created named Type
+        public int TypeId {  get; set; }           // type of word: noun, adjective
+        public string Transcript { get; set; }     // one string for all transcripts: uk - pronunciation1, us - pronunciation2
+
+        public List<(string Meaning, string Example)> MeaningsAndExamples { get; set; }       // meaning of the word - example of use in sentence
+
+        public List <string> Synonyms { get; set; }
+        public List <string> Antonyms { get; set; }
+
+        public Type Type { get; set; }        // navigational property
+    }
+}
