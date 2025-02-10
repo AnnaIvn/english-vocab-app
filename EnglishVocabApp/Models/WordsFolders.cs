bf0dbb967cfa0b5d7f17cb49c0af128633d46c2a
Maxim Gnizdovskiy <145029206+MaxxML2023@users.noreply.github.com>
2025-02-10 11:28:59 +0200

    Add files via upload

---
+++ EnglishVocabApp/Models/WordsFolders.cs
@@ -0,0 +1,9 @@
+﻿namespace EnglishVocabApp.Models
+public class WordsFolders
+{
+    public int WordId { get; set; }
+    public int FolderId { get; set; }
+
+    public Word Word { get; set; }
+    public Folder Folder { get; set; }
+}
