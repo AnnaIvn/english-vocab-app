d7bb53b256621415bb27dacf218748911981c29b
Ivan0707z <112898142+Ivan0707z@users.noreply.github.com>
2025-02-10 10:07:49 +0200

    Add files via upload

---
+++ EnglishVocabApp/Models/Folder.cs
@@ -0,0 +1,13 @@
+﻿public class Folder
+{
+    public int Id { get; set; } // Ідентифікатор папки
+    public string Name { get; set; } // Назва папки
+    public string Description { get; set; } // Опис папки або категорія
+    public int OwnerId { get; set; } // Ідентифікатор користувача, якому належить папка
+    public DateTime CreatedAt { get; set; } // Дата створення папки
+    public DateTime UpdatedAt { get; set; } // Дата останнього оновлення
+    public bool IsPrivate { get; set; } // Чи є папка приватною
+
+    // Посилання на власника папки
+    public User User { get; set; }
+}
