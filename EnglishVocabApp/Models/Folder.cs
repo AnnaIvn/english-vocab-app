1d10ff625a24e455ea4050ba4b8060291cce581f
Maxim Gnizdovskiy <145029206+MaxxML2023@users.noreply.github.com>
2025-02-10 11:11:53 +0200

    Update Folder.cs

--- EnglishVocabApp/Models/Folder.cs
+++ EnglishVocabApp/Models/Folder.cs
@@ -3,7 +3,7 @@
     public int Id { get; set; } // Ідентифікатор папки
     public string Name { get; set; } // Назва папки
     public string Description { get; set; } // Опис папки або категорія
-    public int OwnerId { get; set; } // Ідентифікатор користувача, якому належить папка
+    public int UserId { get; set; } // Ідентифікатор користувача, якому належить папка
     public DateTime CreatedAt { get; set; } // Дата створення папки
     public DateTime UpdatedAt { get; set; } // Дата останнього оновлення
     public bool IsPrivate { get; set; } // Чи є папка приватною
