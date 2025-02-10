4ecf4d4c593a2340801eb3690e5697878dbd2a12
Maxim Gnizdovskiy <145029206+MaxxML2023@users.noreply.github.com>
2025-02-10 13:11:23 +0200

    Add files via upload

---
+++ EnglishVocabApp/Models/User.cs
@@ -0,0 +1,18 @@
+﻿public class User:IdentityUser
+{
+    public string Username { get; set; } // Ім’я користувача
+    public string Email { get; set; } // Адреса електронної пошти
+    public string PasswordHash { get; set; } // Хешований пароль
+    public DateTime DateJoined { get; set; } // Дата створення облікового запису
+
+    // Колекція папок, створених користувачем
+    public List<Folder> CreatedFolders { get; set; } = new List<Folder>();
+
+    // Колекція вивчених слів
+    public List<Word> LearnedWords { get; set; } = new List<Word>();
+
+    // Загальна кількість вивчених слів
+    public int TotalWordsLearned => LearnedWords.Count;
+}
+
+
