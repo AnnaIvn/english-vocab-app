bf4d44298ca0184768fa1c989f556b6f3e6f1a91
Ivanytska Anna <aniaiv12345@gmail.com>
2025-02-24 12:19:43 +0200

    added simple User controller and simple Index view page

---
+++ EnglishVocabApp/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using EnglishVocabApp.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnglishVocabApp.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UsersController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var users = _db.Users.ToList();
+
+            return View(users);
+        }
+    }
+}
--- EnglishVocabApp/Models/User.cs
+++ EnglishVocabApp/Models/User.cs
@@ -7,11 +7,11 @@ namespace EnglishVocabApp.Models
     {
         public DateTime? DateJoined { get; set; }                                 // Дата створення облікового запису
 
-        public List<Folder> CreatedFolders { get; set; } = new List<Folder>();    // Колекція папок, створених користувачем
+        public List<Folder>? CreatedFolders { get; set; } = new List<Folder>();    // Колекція папок, створених користувачем
 
-        public List<Word> LearnedWords { get; set; } = new List<Word>();          // Колекція вивчених слів
+        public List<Word>? LearnedWords { get; set; } = new List<Word>();          // Колекція вивчених слів
 
-        public int TotalWordsLearned => LearnedWords?.Count ?? 0;                 // Загальна кількість вивчених слів   // will work even if it is null
+        public int? TotalWordsLearned => LearnedWords?.Count ?? 0;                 // Загальна кількість вивчених слів   // will work even if it is null
                                                                                   // public string PasswordHash { get; set; } // Хешований пароль  // password is in User, i'll coment this for now
                                                                                   // ? - to be able for these fields to be null, because they aren't implemented right now in any way
 
