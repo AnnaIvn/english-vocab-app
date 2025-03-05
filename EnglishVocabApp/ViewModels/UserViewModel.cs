fcfca160f3425fe357df7b87b7b0411c45dbdc01
MaxxML2023 <ajefdjdidk.thg@gmail.com>
2025-03-05 12:57:19 +0200

    Добавил UserViewModel

---
+++ EnglishVocabApp/ViewModels/UserViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace EnglishVocabApp.ViewModels
+{
+    public class UserViewModel
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public DateTime? DateJoined { get; set; }
+        public int TotalWordsLearned { get; set; }
+
+        public UserViewModel() { }
+
+        public UserViewModel(Models.User user)
+        {
+            Id = user.Id;
+            UserName = user.UserName;
+            DateJoined = user.DateJoined;
+            TotalWordsLearned = user.TotalWordsLearned ?? 0;
+        }
+    }
+}
