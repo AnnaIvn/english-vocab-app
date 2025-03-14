dc52e2853a7df9cf6042438b57a1ebeb50fffd70
Ivanytska Anna <aniaiv12345@gmail.com>
2025-03-14 16:24:25 +0200

    added ModelValidationController and wrote action for checking if name of class Type is unique + added translation of message for this case

---
+++ EnglishVocabApp/Controllers/ModelValidationController.cs
@@ -0,0 +1,20 @@
+﻿using EnglishVocabApp.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnglishVocabApp.Controllers
+{
+    public class ModelValidationController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public ModelValidationController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult CheckNewTypeName(string name)
+        {
+            var result = _context.Types.Any(t => t.Name == name);   
+            return Json(!result);
+        }
+    }
+}
--- EnglishVocabApp/ViewModels/TypeViewModel.cs
+++ EnglishVocabApp/ViewModels/TypeViewModel.cs
@@ -1,12 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EnglishVocabApp.ViewModels
 {
     public class TypeViewModel
     {
         public int Id { get; set; }
+
         [Required]
         [Display(Name = "lblName")]
+        [Remote("CheckNewTypeName", "ModelValidation", ErrorMessage ="lblTypeNameMustBeUnique")]
         public string Name { get; set; }
 
         public TypeViewModel() { }
