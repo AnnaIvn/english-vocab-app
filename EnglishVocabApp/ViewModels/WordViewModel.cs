5ebc3056c2d546368ac2ddcfe43f8d145e4124f8
Ivanytska Anna <aniaiv12345@gmail.com>
2025-03-14 15:31:20 +0200

    added error messages/hints to WordViewModel.cs

--- EnglishVocabApp/ViewModels/WordViewModel.cs
+++ EnglishVocabApp/ViewModels/WordViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using EnglishVocabApp.Models;
 
@@ -7,8 +8,17 @@ namespace EnglishVocabApp.ViewModels
     public class WordViewModel
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "lblNameOfTheWordIsRequired")]
+        [StringLength(45, MinimumLength = 1, ErrorMessage = "lblNameLengthMessage")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "lblTranscriptOfTheWordIsRequired")]
+        [StringLength(50, MinimumLength = 1)]
         public string Transcript { get; set; }
+
+        [Required(ErrorMessage = "lblMeaningOfTheWordIsRequired")]
+        [StringLength(200, MinimumLength = 5)]
         public string Meaning { get; set; }
         public string ExamplesString { get; set; }
         public string SynonymsString { get; set; }
@@ -18,6 +28,7 @@ namespace EnglishVocabApp.ViewModels
         public List<string> Synonyms { get; set; } = new List<string>();
         public List<string> Antonyms { get; set; } = new List<string>();
 
+        [Required(ErrorMessage = "lblTypeOfTheWordIsRequired")]
         public string TypeName { get; set; }
 
         public WordViewModel() { }
